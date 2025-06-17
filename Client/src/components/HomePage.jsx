1fd92db32e5387e709db5c87c636bc946997d319
Nguyen Tri <ntri86679@gmail.com>
2025-06-17 10:24:31 +0700

    chia route

---
+++ Client/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the main landing page of our application.</p>
+        {/* Add more content or components as needed */}
+        </div>
+    );
+}
+
+export default HomePage;
--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <StrictMode>
+  {/*Render giao diện của trang web */}
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </StrictMode>
 )
